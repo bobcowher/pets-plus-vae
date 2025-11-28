d3fe6ef1049519742c2670571e09eb06d8b8f8f9
Robert Cowher <bobcowher@gmail.com>
2025-11-28 12:23:14 -0600

    Basic encoder working

--- agent.py
+++ agent.py
@@ -1,4 +1,7 @@
 import gymnasium as gym
+from model import VAE 
+import torch
+import cv2
 
 class Agent:
 
@@ -9,6 +12,18 @@ class Agent:
             render_mode = "rgb_array"
 
         self.env = gym.make("CarRacing-v3", render_mode=render_mode, lap_complete_percent=0.95, domain_randomize=False, continuous=False)
+        
+        self.VAE = VAE(observation_shape=(64, 64, 3))
+
+
+    def process_observation(self, obs):
+        # obs = obs.cpu().numpy()
+
+        obs = cv2.resize(obs, (64, 64), interpolation=cv2.INTER_NEAREST)
+
+        obs = torch.from_numpy(obs).float().permute(2, 0, 1)
+
+        return obs 
 
 
     def train(self, episodes=1):
@@ -16,6 +31,10 @@ class Agent:
         for episode in range(episodes):
             done = False
             obs, info = self.env.reset()
+
+            obs = self.process_observation(obs)
+
+            pred, enc = self.VAE(obs)
             
             while not done:
                 action = self.env.action_space.sample()
---
+++ model.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class VAE(nn.Module):
+
+    def __init__(self, observation_shape=()):
+        super().__init__()
+
+        # print(observation_shape[-1])
+        conv_output_dim = 64
+
+        self.conv1 = nn.Conv2d(observation_shape[-1], 32, kernel_size=8, stride=4, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=2, stride=1, padding=1)
+
+        self.compression = nn.Linear(conv_output_dim, conv_output_dim)
+
+        self.flatten = torch.nn.Flatten()
+
+        self.deconv1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=1, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=1, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
+        self.deconv4 = nn.ConvTranspose2d(32, observation_shape[-1], kernel_size=8, stride=4, padding=2)
+        # self.conv3 = nn.Conv2d()
+
+        print("Network initialized...")
+
+    def _conv_forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        
+        return x
+
+
+    def _deconv_forward(self, x):
+        x = F.relu(self.deconv1(x))
+        x = F.relu(self.deconv2(x))
+        x = F.relu(self.deconv3(x))
+        x = F.relu(self.deconv4(x))
+        
+        return x
+        
+    
+    def forward(self, x):
+        x = x / 255.0
+
+        print(x.mean())
+
+        x = self._conv_forward(x)
+    
+        print(x)
+
+        # x = self.flatten(x)
+
+        # x = self.compression(x)
+
+        # Saving off the compression layer as an output
+        enc = x
+
+        # x = self._deconv_forward(x)
+
+        return x, enc 
+
+
+
+
+
+
+
+
+        
+
